50163f432eb91522ae9e9708e10fc4e957f97508
JoJo <18170365@itculiacan.edu.mx>
2023-09-26 21:58:33 -0700

    Add problem C

---
+++ BasicElements/NumeroAlrevez.cs
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class NumeroAlrevez {
+    
+    
+  static void Main() {
+      int Uno, Dos, Tres;
+      Uno = Convert.ToInt32(Console.ReadLine());
+      Dos = Convert.ToInt32(Console.ReadLine());
+      Tres = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Tres);
+    Console.WriteLine(Dos);
+    Console.WriteLine(Uno);
+  }
+}
